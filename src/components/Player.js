69a2ba2c26ca52e316b89012085056b29fe48b6f
pramit-marattha <pramitmarattha5497@gmail.com>
2021-10-18 21:01:18 +0545

    styles updated

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,65 +1,78 @@
-import React,{useState,useRef,useEffect} from 'react';
+import React, { useState, useRef, useEffect } from "react";
 import PlayerDetails from "./PlayerDetails";
 import PlayerControls from "./PlayerControls";
 
 function Player(props) {
-    const audioElement = useRef(null);
-    const [isPlaying, setIsPlaying] = useState(false);
+  const audioElement = useRef(null);
+  const [isPlaying, setIsPlaying] = useState(false);
 
-    useEffect(() => {
-        if (isPlaying) {
-            audioElement.current.play();
-        } else {
-            audioElement.current.pause();
-        }
-    });
-
-    const SkipSong = (forwards = true) => {
-        if (forwards) {
-            props.setCurrentSongIndex(() => {
-                let temp = props.currentSongIndex;
-                temp++;
-
-                if (temp > props.songs.length - 1) {
-                    temp = 0;
-                }
-
-                return temp;
-            });
-        } else {
-            props.setCurrentSongIndex(() => {
-                let temp = props.currentSongIndex;
-                temp--;
-
-                if (temp < 0) {
-                    temp = props.songs.length - 1;
-                }
-
-                return temp;
-            });
-        }
+  useEffect(() => {
+    if (isPlaying) {
+      audioElement.current.play();
+    } else {
+      audioElement.current.pause();
     }
+  });
 
+  const SkipSong = (forwards = true) => {
+    if (forwards) {
+      props.setCurrentSongIndex(() => {
+        let temp = props.currentSongIndex;
+        temp++;
 
+        if (temp > props.songs.length - 1) {
+          temp = 0;
+        }
 
-    return (
-        <div className="c-player">
-        <audio src={props.songs[props.currentSongIndex].src} ref={audioElement}>
-        </audio>
-        <h4>Lofi Music Player React </h4>
+        return temp;
+      });
+    } else {
+      props.setCurrentSongIndex(() => {
+        let temp = props.currentSongIndex;
+        temp--;
 
-        <PlayerDetails song={props.songs[props.currentSongIndex]} />
-        <PlayerControls isPlaying={isPlaying} setIsPlaying={setIsPlaying} SkipSong={SkipSong}/>
-        <p>
+        if (temp < 0) {
+          temp = props.songs.length - 1;
+        }
+
+        return temp;
+      });
+    }
+  };
+
+  return (
+    <div className="music-player">
+      <audio
+        src={props.songs[props.currentSongIndex].src}
+        ref={audioElement}
+      ></audio>
+      <h4>Lofi Music Player React </h4>
+
+      <PlayerDetails song={props.songs[props.currentSongIndex]} />
+      <PlayerControls
+        isPlaying={isPlaying}
+        setIsPlaying={setIsPlaying}
+        SkipSong={SkipSong}
+      />
+      <p>
         <div className="text-anim">
-        <strong>Next Song:</strong>
+          <strong>Upcoming Song:</strong>
         </div>
-         <div className="nextsong-details-img">
-         <img src={props.songs[props.nextSongIndex].img_src} alt={props.songs[props.nextSongIndex].title} style={{width:"4em", height:"auto" }}/>
-         </div>
-         {props.songs[props.nextSongIndex].title} by <b>{props.songs[props.nextSongIndex].artist}</b> from album <b>{props.songs[props.nextSongIndex].album}</b>
-         </p>
+        <div className="nextsong-details">
+          <img
+            src={props.songs[props.nextSongIndex].img_src}
+            alt={props.songs[props.nextSongIndex].title}
+            style={{ width: "4em", height: "auto" }}
+          />
+          <p>
+            <b>{props.songs[props.nextSongIndex].title} </b>&nbsp; by &nbsp;
+            <b>{props.songs[props.nextSongIndex].artist}</b>&nbsp; from album
+            &nbsp;
+            <b>{props.songs[props.nextSongIndex].album}</b>
+          </p>
         </div>
-    )
+      </p>
+    </div>
+  );
 }
 export default Player;
--- src/components/PlayerDetails.js
+++ src/components/PlayerDetails.js
@@ -1,15 +1,15 @@
-import React from 'react'
+import React from "react";
 
 function PlayerDetails(props) {
-    return (
-        <div className="c-player--details">
-        <div className="details-img">
-            <img src={props.song.img_src} alt={props.song.title}/>
-        </div>
-        <h3 className="details-title">{props.song.title}</h3>
-        <h4 className="details-artist">{props.song.artist}</h4>
-        </div>
-    )
+  return (
+    <div className="music-player--details">
+      <div className="details-img">
+        <img src={props.song.img_src} alt={props.song.title} />
+      </div>
+      <h3 className="details-title">{props.song.title}</h3>
+      <h4 className="details-artist">{props.song.artist}</h4>
+    </div>
+  );
 }
 
-export default PlayerDetails
+export default PlayerDetails;
